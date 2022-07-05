35d807548701724836a3d9d1cd554a44fed922b5
Spiro Sideris <tools@spirosideris.com>
2022-07-05 10:30:36 -0400

    Add correct api base url

--- src/api.constants.ts
+++ src/api.constants.ts
@@ -1,3 +1,3 @@
 export const TRAKON_API_BASE =
-  process.env.TRAKON_API_BASE ?? 'https://api.zepherus.xyz/api/v1'
+  process.env.TRAKON_API_BASE ?? 'https://api.trakon.xyz/api/v1'
 export const TRAKON_UI_BASE = process.env.TRAKON_UI_BASE ?? 'https://trakon.xyz'
